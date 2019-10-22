af552df016f5ed849668b9709fb0548bf6ab7b50
wangkui0508 <wangkui0508@hotmail.com>
2019-10-23 04:24:47 +0800

    Add another Consumer implementation TradeConsumerWithDirTail

--- server/consumer.go
+++ server/consumer.go
@@ -1,11 +1,14 @@
 package server
 
 import (
+	"strings"
 	"sync"
 
 	"github.com/Shopify/sarama"
-	"github.com/coinexchain/trade-server/core"
 	log "github.com/sirupsen/logrus"
+
+	"github.com/coinexchain/dirtail"
+	"github.com/coinexchain/trade-server/core"
 )
 
 type Consumer interface {
@@ -115,5 +118,62 @@ func (tc *TradeConsumer) Close() {
 		}
 	}
 
+	log.Info("Consumer close")
+}
+
+// ======================================
+type TradeConsumerWithDirTail struct {
+	dirName    string
+	filePrefix string
+	dt         *dirtail.DirTail
+	hub        *core.Hub
+	writer     MsgWriter
+}
+
+func NewConsumerWithDirTail(dirs []string, filePrefix string, writeConfig string, hub *core.Hub) Consumer {
+	var writer MsgWriter
+	var err error
+	if len(writeConfig) != 0 {
+		if writer, err = NewFileMsgWriter(writeConfig); err != nil {
+			log.WithError(err).Fatal("create writer error")
+		}
+	}
+	return &TradeConsumerWithDirTail{
+		dirName:    dirs[0],
+		filePrefix: filePrefix,
+		hub:        hub,
+		writer:     writer,
+	}
+}
+
+func (tc *TradeConsumerWithDirTail) Consume() {
+	offset := tc.hub.LoadOffset(0)
+	fileOffset := uint32(offset)
+	fileNum := uint32(offset >> 32)
+	tc.dt = dirtail.NewDirTail(tc.dirName, tc.filePrefix, "", fileNum, fileOffset)
+	tc.dt.Start(600, func(line string, fileNum uint32, fileOffset uint32) {
+		offset := (int64(fileNum) << 32) | int64(fileOffset)
+		tc.hub.UpdateOffset(0, offset)
+
+		divIdx := strings.Index(line, "#")
+		key := line[:divIdx]
+		value := []byte(line[divIdx+1:])
+		tc.hub.ConsumeMessage(key, value)
+		if tc.writer != nil {
+			if err := tc.writer.WriteKV([]byte(key), value); err != nil {
+				log.WithError(err).Error("write file failed")
+			}
+		}
+		log.WithFields(log.Fields{"key": key, "value": string(value), "offset": offset}).Debug("consume message")
+	})
+}
+
+func (tc *TradeConsumerWithDirTail) Close() {
+	tc.dt.Stop()
+	if tc.writer != nil {
+		if err := tc.writer.Close(); err != nil {
+			log.WithError(err).Error("file close failed")
+		}
+	}
 	log.Info("Consumer close")
 }
